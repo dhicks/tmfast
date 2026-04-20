3a93b83cc2ae8939aec4ac0fa191abcc59af6fa3
Dan Hicks <hicks.daniel.j@gmail.com>
2026-04-20 14:05:57 -0700

    drat repo was actually just for CRAN; most people can install directly from GH

--- R/zzz.R
+++
@@ -1,7 +0,0 @@
-.onLoad = function(libname, pkgname) {
-      ## This drat repo is used to host the data for the realbooks vignette
-      repos = getOption('repos')
-      repos['tmfast.realbooks'] = 'https://dhicks.github.io/drat/'
-      options(repos = repos)
-      invisible(repos)
-}
